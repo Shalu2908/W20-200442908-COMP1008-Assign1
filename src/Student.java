47cbbac91b0db891e8ea4a57be66b0be05201c0e
Shalu2908 <56652998+Shalu2908@users.noreply.github.com>
2020-01-17 13:29:03 -0500

    Added Constructor without validation

--- src/Student.java
+++ src/Student.java
@@ -4,5 +4,10 @@ public class Student {
 
     private String firstName, lastName;
     private int studentNumber;
-    
+
+    public Student(String firstName, String lastName, int studentNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+    }
 }
