492abce39b144c83ae6e425db41bc9dcc7b6206d
Shalu2908 <56652998+Shalu2908@users.noreply.github.com>
2020-01-25 18:11:56 -0500

    Added Constructor with validation, Public get/set methods, a toString method and Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Student Student1 = new Student ("shalu","sharma",200442908) ;
+
+        System.out.printf("%s %n",Student1);
+
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,63 @@
-public class Student {
+ public class Student
+ {
 
-    // instance variables for a student
+    // These are instance variables for a student
+    //They are marked as private so that ONLY the student class can control how they are
+    //set
 
     private String firstName, lastName;
     private int studentNumber;
 
+    /**
+     * This is the constructor.  It's goal is to validate the arguments and set
+     * valid values in the instance variables
+     *  eg. firstName,LastName and studentNumber
+     */
     public Student(String firstName, String lastName, int studentNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.studentNumber = studentNumber;
+        setFirstName(firstName);
+        setLastName (lastName);
+        setStudentNumber(studentNumber);
     }
-}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+
+     if( firstName.length() > 1)
+        this.firstName = firstName ;
+     else
+         throw new IllegalArgumentException("Student first Name should be more than one characters");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+
+        if( lastName.length() > 1)
+        this.lastName = lastName;
+        else
+            throw new IllegalArgumentException("Student's last Name should be more than one characters");
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        if (studentNumber >= 100000000 && studentNumber <= 999999999)
+            this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException("Student Number must be in between 100000000 to 999999999");
+    }
+        public String toString()
+        {
+            return String.format("%s %s Student #: %d", firstName.substring(0,1).toUpperCase()+firstName.substring(1),lastName.substring(0,1).toUpperCase()+lastName.substring(1),
+                    studentNumber);
+        }
+
+
+ }
