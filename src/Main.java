924df89e27ff615e3456d47f38bedf4354343eef
Shalu2908 <56652998+Shalu2908@users.noreply.github.com>
2020-01-30 14:54:05 -0500

    Added Constructor with validation, Public get/set methods, a toString method and Main class add some changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args){
 
+        /**
+         * Initiated the main method with Student's information which will be printed to console by toString()
+         * in the Student class
+         */
+
         Student Student1 = new Student ("shalu","sharma",200442908) ;
 
         System.out.printf("%s %n",Student1);
--- src/Student.java
+++ src/Student.java
@@ -19,13 +19,18 @@
         setStudentNumber(studentNumber);
     }
 
+     /**
+      * These are get/set methods for each instance variables
+      * @return
+      */
+
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
 
-     if( firstName.length() > 1)
+     if( firstName.length() > 2)
         this.firstName = firstName ;
      else
          throw new IllegalArgumentException("Student first Name should be more than one characters");
@@ -37,7 +42,7 @@
 
     public void setLastName(String lastName) {
 
-        if( lastName.length() > 1)
+        if( lastName.length() > 2)
         this.lastName = lastName;
         else
             throw new IllegalArgumentException("Student's last Name should be more than one characters");
@@ -53,10 +58,15 @@
         else
             throw new IllegalArgumentException("Student Number must be in between 100000000 to 999999999");
     }
+
+     /**
+      *  A toString() method that returns a String in form of firstName lastName and student #: 123456
+      * @return
+      */
         public String toString()
         {
-            return String.format("%s %s Student #: %d", firstName.substring(0,1).toUpperCase()+firstName.substring(1),lastName.substring(0,1).toUpperCase()+lastName.substring(1),
-                    studentNumber);
+            return String.format("%s %s Student #: %d", firstName.substring(0,1).toUpperCase()+firstName.substring(1),
+                    lastName.substring(0,1).toUpperCase()+lastName.substring(1), studentNumber);
         }
 
 
