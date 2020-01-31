3ada358ffb6ecbf2f11e9db5e304b083dd4f6d01
Shalu2908 <56652998+Shalu2908@users.noreply.github.com>
2020-01-31 13:26:54 -0500

    Modify the main class, added a FXML file and a controller class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+    /**
+     * This extends Application and add the start () method
+     */
     public static void main(String[] args){
+        launch(args);
 
         /**
          * Initiated the main method with Student's information which will be printed to console by toString()
@@ -12,4 +21,15 @@ public class Main {
         System.out.printf("%s %n",Student1);
 
     }
+
+    @Override
+    public void start (Stage window) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("StudentView.fxml"));
+        Scene scene = new Scene(root);
+        window.setScene(scene);
+        window.setTitle("Student Card");
+        window.show();
+    }
+
 }
